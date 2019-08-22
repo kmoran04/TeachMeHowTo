dfdae98be0c80a853e58ca59e20c400837737368
kmoran04 <kristen.moran@tufts.edu>
2019-08-22 12:58:47 -0400

    Updates

---
+++ precache-manifest.51e789eea2acc30acd730686af89deaa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ef42a3af9a0176410c248d692429399",
+    "url": "/TeachMeHowTo/index.html"
+  },
+  {
+    "revision": "86e0827ceeab45046f75",
+    "url": "/TeachMeHowTo/static/css/main.8487fe1f.chunk.css"
+  },
+  {
+    "revision": "21d232bc464b5e61ecd1",
+    "url": "/TeachMeHowTo/static/js/2.49072c96.chunk.js"
+  },
+  {
+    "revision": "86e0827ceeab45046f75",
+    "url": "/TeachMeHowTo/static/js/main.51b733b7.chunk.js"
+  },
+  {
+    "revision": "a400608c8ce8ffafc3a5",
+    "url": "/TeachMeHowTo/static/js/runtime~main.3f7571cc.js"
+  },
+  {
+    "revision": "a33f8036b6dcdea310f03d2781b1fcb5",
+    "url": "/TeachMeHowTo/static/media/Cogo-Connect-Icon.a33f8036.png"
+  },
+  {
+    "revision": "6767d9e0819d6907c7e042809f6d2020",
+    "url": "/TeachMeHowTo/static/media/icon_slack_hash_colored.6767d9e0.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TeachMeHowTo/static/media/logo.5d5d9eef.svg"
+  }
+]);
